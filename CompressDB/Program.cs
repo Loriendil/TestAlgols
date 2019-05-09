ac4c2c0773373db6e306074b500a3eb1f958eb36
Kirill Krylov <yellowflashlight@yandex.ru>
2019-05-10 01:45:43 +0300

    Added overrided Equals() method and simple comparation.

--- CompressDB/Program.cs
+++ CompressDB/Program.cs
@@ -33,7 +33,7 @@ namespace CompressDB
             List<Standard> TTable = new List<Standard>();
             CreateVolAndTestLists(table, out VTable, out TTable);
             List<Standard> output = CompressedDB(VTable, TTable);
-
+            Console.ReadKey();
         }
 
         /*
@@ -63,7 +63,8 @@ namespace CompressDB
                 var buffer = new Standard();
                 var fbuffer = buffer.GetStandard
                     (
-                            (t) => {
+                            (t) =>
+                            {
                                 t.TRclause = line[0];
                                 t.TRnum = line[1];
                                 t.STclauseV = line[2];
@@ -83,7 +84,8 @@ namespace CompressDB
                 var buffer = new Standard();
                 var fbuffer = buffer.GetStandard
                     (
-                            (t) => {
+                            (t) =>
+                            {
                                 t.TRclause = ln[0];
                                 t.TRnum = ln[1];
                                 t.STclauseT = ln[2];
@@ -104,110 +106,184 @@ namespace CompressDB
         {
             List<Standard> compressedDB = new List<Standard>();
             // TODO: Productive action! I don't know what is it. 
-
-            bool equalAB = VolTable.SequenceEqual(TestTable, new StandardComparer());
-            Console.WriteLine("Equal?" + equalAB);
+            foreach (var line in VolTable)
+            {
+                foreach (var ln in TestTable)
+                {
+                    if (ln.Equals(line))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+            }
+            //bool equalAB = VolTable.SequenceEqual(TestTable, new StandardComparer());
+            //Console.WriteLine("Equal?" + equalAB);
             return compressedDB;
         }
-    }
-    public class Standard
-    {
-        #region field for properties
-        private string tRclause = string.Empty;
-        private string tRnum = string.Empty;
-        private string sTclauseV = string.Empty;
-        private string sTclauseT = string.Empty;
-        private string sTnum = string.Empty;
-        private string sTname = string.Empty;
-        private string comment = string.Empty;
-        private string lType = string.Empty;
-        private string lComGrp = string.Empty;
-        private string lSpGrp = string.Empty;
-        private string sTsource = string.Empty;
-        private string expirDateTR = string.Empty;
-        private string expirDateFSA = string.Empty;
-        private string expirDatePubl = string.Empty;
-        #endregion
 
-        #region Properties 
-        public string TRclause { get => tRclause; set => tRclause = value; }
-        public string TRnum { get => tRnum; set => tRnum = value; }
-        public string STclauseV { get => sTclauseV; set => sTclauseV = value; }
-        public string STclauseT { get => sTclauseT; set => sTclauseT = value; }
-        public string STnum { get => sTnum; set => sTnum = value; }
-        public string STname { get => sTname; set => sTname = value; }
-        public string Comment { get => comment; set => comment = value; }
-        public string ExpirDateTR { get => expirDateTR; set => expirDateTR = value; }
-        public string ExpirDateFSA { get => expirDateFSA; set => expirDateFSA = value; }
-        public string ExpirDatePubl { get => expirDatePubl; set => expirDatePubl = value; }
-        public string LType { get => lType; set => lType = value; }
-        public string LComGrp { get => lComGrp; set => lComGrp = value; }
-        public string LSpGrp { get => lSpGrp; set => lSpGrp = value; }
-        public string STsource { get => sTsource; set => sTsource = value; }
-        #endregion
-
-        /// <summary>
-        /// This method gives an ability to fill properties of custom class by one click
-        /// </summary>
-        /// <param name="alteration"> a set of all properties</param>
-        /// <returns>delegate with set of properties</returns>
-        public Standard GetStandard(Action<Standard> alteration)
+        public static void Compress()
         {
-            alteration(this);
-            return this;
+            Standard s1 = new Standard();
+
+            s1.TRclause = "статья 4";
+            s1.TRnum = "ТР ТС 004/2011";
+            s1.STclauseV = "раздел 1";
+            s1.STclauseT = string.Empty;
+            s1.STnum = "ГОСТ 15047 - 78";
+            s1.STname = "Электроприборы нагревательные бытовые.Термины и определения";
+            s1.Comment = "применяется до 31.09.2017";
+            s1.ExpirDateTR = string.Empty;
+            s1.ExpirDateFSA = string.Empty;
+            s1.ExpirDatePubl = string.Empty;
+            s1.LType = "free";
+            s1.LComGrp = string.Empty;
+            s1.LSpGrp = string.Empty;
+            s1.STsource = "RU";
+
+            Standard s2 = new Standard();
+
+            s2.TRclause = "статья 4";
+            s2.TRnum = "ТР ТС 004/2011";
+            s2.STclauseV = string.Empty;
+            s2.STclauseT = "раздел 6" ;
+            s2.STnum = " ГОСТ Р IEC 60745 - 2 - 13 - 2012 ";
+            s2.STname = "Машины ручные электрические. Безопасность и методы испытаний. Часть 2 - 13.Частные требования к цепным пилам ";
+            s2.Comment = string.Empty;
+            s2.ExpirDateTR = string.Empty;
+            s2.ExpirDateFSA = string.Empty;
+            s2.ExpirDatePubl = string.Empty;
+            s2.LType = "test";
+            s2.LComGrp = string.Empty;
+            s2.LSpGrp = string.Empty;
+            s2.STsource = "RU";
+
+            Console.WriteLine(s1.Equals(s2));
+
+            Console.ReadKey();
+
+
         }
-
-        public static string TemplatedString(string line)
+        public class Standard : IEquatable<Standard>
         {
-            line = line.Trim();
-            char[] foo = line.ToCharArray();
-            StringBuilder outp = new StringBuilder();
-            for (int i = 0; i < foo.Length; i++)
+            #region field for properties
+            private string tRclause = string.Empty;
+            private string tRnum = string.Empty;
+            private string sTclauseV = string.Empty;
+            private string sTclauseT = string.Empty;
+            private string sTnum = string.Empty;
+            private string sTname = string.Empty;
+            private string comment = string.Empty;
+            private string lType = string.Empty;
+            private string lComGrp = string.Empty;
+            private string lSpGrp = string.Empty;
+            private string sTsource = string.Empty;
+            private string expirDateTR = string.Empty;
+            private string expirDateFSA = string.Empty;
+            private string expirDatePubl = string.Empty;
+            #endregion
+
+            #region Properties 
+            public string TRclause { get => tRclause; set => tRclause = value; }
+            public string TRnum { get => tRnum; set => tRnum = value; }
+            public string STclauseV { get => sTclauseV; set => sTclauseV = value; }
+            public string STclauseT { get => sTclauseT; set => sTclauseT = value; }
+            public string STnum { get => sTnum; set => sTnum = value; }
+            public string STname { get => sTname; set => sTname = value; }
+            public string Comment { get => comment; set => comment = value; }
+            public string ExpirDateTR { get => expirDateTR; set => expirDateTR = value; }
+            public string ExpirDateFSA { get => expirDateFSA; set => expirDateFSA = value; }
+            public string ExpirDatePubl { get => expirDatePubl; set => expirDatePubl = value; }
+            public string LType { get => lType; set => lType = value; }
+            public string LComGrp { get => lComGrp; set => lComGrp = value; }
+            public string LSpGrp { get => lSpGrp; set => lSpGrp = value; }
+            public string STsource { get => sTsource; set => sTsource = value; }
+            #endregion
+
+            /// <summary>
+            /// This method gives an ability to fill properties of custom class by one click
+            /// </summary>
+            /// <param name="alteration"> a set of all properties</param>
+            /// <returns>delegate with set of properties</returns>
+            /// 
+            public Standard GetStandard(Action<Standard> alteration)
             {
-                if (char.IsLetterOrDigit(foo[i]))
-                {
-                    outp.Append(foo[i]);
-                }
-                if (char.IsPunctuation(foo[i]))
-                {
-                    outp.Append(foo[i]);
-                }
-                if (char.IsWhiteSpace(foo[i]))
-                {
-                    if (char.IsWhiteSpace(foo[i - 1])) { continue; }
-                    if (char.IsPunctuation(foo[i - 1]) || char.IsPunctuation(foo[i + 1])) { continue; }
-                    else { outp.Append(foo[i]); }
-                }
-
+                alteration(this);
+                return this;
             }
-            return outp.ToString();
+
+            public static string TemplatedString(string line)
+            {
+                line = line.Trim();
+                char[] foo = line.ToCharArray();
+                StringBuilder outp = new StringBuilder();
+                for (int i = 0; i < foo.Length; i++)
+                {
+                    if (char.IsLetterOrDigit(foo[i]))
+                    {
+                        outp.Append(foo[i]);
+                    }
+                    if (char.IsPunctuation(foo[i]))
+                    {
+                        outp.Append(foo[i]);
+                    }
+                    if (char.IsWhiteSpace(foo[i]))
+                    {
+                        if (char.IsWhiteSpace(foo[i - 1])) { continue; }
+                        if (char.IsPunctuation(foo[i - 1]) || char.IsPunctuation(foo[i + 1])) { continue; }
+                        else { outp.Append(foo[i]); }
+                    }
+
+                }
+                return outp.ToString();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return this.Equals(obj as Standard);
+            }
+
+            public bool Equals(Standard other)
+            {
+                if (other == null)
+                { return false; }
+                return this.STnum.Equals(other.STnum);
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
         }
-    }
 
-    class StandardComparer : IEqualityComparer<Standard>
-    {
-        public bool Equals(Standard x, Standard y)
+        class StandardComparer : IEqualityComparer<Standard>
         {
-            //Check whether the compared objects reference the same data.
-            if (Object.ReferenceEquals(x, y)) return true;
+            public bool Equals(Standard x, Standard y)
+            {
+                //Check whether the compared objects reference the same data.
+                if (Object.ReferenceEquals(x, y)) return true;
 
-            //Check whether any of the compared objects is null.
-            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
-                return false;
-            //Check whether the products' properties are equal.
-            return x.STnum == y.STnum;
-        }
+                //Check whether any of the compared objects is null.
+                if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                    return false;
+                //Check whether the products' properties are equal.
+                return x.STnum == y.STnum;
+            }
 
-        public int GetHashCode(Standard standard)
-        {
-            //Check whether the object is null
-            if (Object.ReferenceEquals(standard, null)) return 0;
+            public int GetHashCode(Standard standard)
+            {
+                //Check whether the object is null
+                if (Object.ReferenceEquals(standard, null)) return 0;
 
-            //Get hash code for the Code field.
-            int hashStandardNum = standard.STnum.GetHashCode();
+                //Get hash code for the Code field.
+                int hashStandardNum = standard.STnum.GetHashCode();
 
-            //Calculate the hash code for the product.
-            return hashStandardNum;
+                //Calculate the hash code for the product.
+                return hashStandardNum;
+            }
         }
     }
 }
